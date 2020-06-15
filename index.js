edc098d181b77a3c43c44ce89f8386e6ca80e7cc
Felipe Rodrigo de Souza Gonçalves <f_g@outlook.com.br>
2020-06-15 16:03:26 -0300

    Cálculo de média (aula2 e aula3)

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-const nome = 'KI0T0 Bootcamp'
-console.log(nome)
+/*const nome = 'KI0T0 Bootcamp'
+console.log(nome)*/
+
+
+//Criar um programa que calcula a média
+//das notas entre os alunos e envia
+//mensagem do cálculo da média.
+
+const aluno01 = 'Mayk'
+const aluno02 = 'Diego'
+const aluno03 = 'Fulano'
+const notaAluno01 = 9.8
+const notaAluno02 = 10
+const notaAluno03 = 2
+
+const media = (notaAluno01 + notaAluno02 + notaAluno03)/3
+
+console.log(media)
