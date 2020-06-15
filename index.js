7a53a79041e403492bcfb25286113b4ec25db57c
Felipe Rodrigo de Souza Gonçalves <f_g@outlook.com.br>
2020-06-15 16:14:37 -0300

    Condicional (aula 5)

--- index.js
+++ index.js
@@ -7,12 +7,26 @@ console.log(nome)*/
 //mensagem do cálculo da média.
 
 const aluno01 = 'Mayk'
-const aluno02 = 'Diego'
-const aluno03 = 'Fulano'
 const notaAluno01 = 9.8
+const aluno02 = 'Diego'
 const notaAluno02 = 10
+const aluno03 = 'Fulano'
 const notaAluno03 = 2
+
+
 
 const media = (notaAluno01 + notaAluno02 + notaAluno03)/3
 
-console.log(media)
+
+//Se a média for maior que 5, parabenizar a turma
+
+
+if (media > 5){
+    console.log(`A média da turma foi de ${media}... Parabéns!`)
+
+
+}else{
+    console.log('A média da turma foi menor que 5')
+
+}
+
