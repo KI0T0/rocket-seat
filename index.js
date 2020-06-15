f74433fa6c8449c3ad3521334570c1c29c1efcec
Felipe Rodrigo de Souza Gonçalves <f_g@outlook.com.br>
2020-06-15 16:38:32 -0300

    Operadores relacionais e comparativos (aula6)

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log(nome)*/
 //das notas entre os alunos e envia
 //mensagem do cálculo da média.
 
-const aluno01 = 'Mayk'
+/*const aluno01 = 'Mayk'
 const notaAluno01 = 9.8
 const aluno02 = 'Diego'
 const notaAluno02 = 10
@@ -28,5 +28,53 @@ if (media > 5){
 }else{
     console.log('A média da turma foi menor que 5')
 
+}
+*/
+
+
+/*
+================================================
+
+OPERADORES DE COMPARAÇÃO
+
+>           Maior
+<           Menor
+>=          Maior ou igual a
+<=          Menor ou igual a
+==          Igual a
+===         Igual e do mesmo tipo
+!=          Diferente de
+!==         Diferente, inclusive do tipo
+
+=================================================*/
+
+console.log(5 > 4) //true
+console.log(5 < 4) //false
+console.log(5 >= 4) //true
+console.log(4 <= 4) //true
+
+console.log(4 == '4') //true
+console.log(4 === '4') //false
+console.log(4 != '5') //true
+console.log(4 !== '5') //true
+
+
+//DESAFIO 1
+
+const idade = 17
+
+//verificar se a pessoa é maior ou igual a 18 anos
+//se sim, deixar a pessoa entrar, se não, bloquear a entrada
+if (idade>=18){
+    console.log('Deixar entrar')
+}else{
+    console.log('Bloquear a entrada')
+}
+
+//se a pessoa tiver 17 anos
+//avisar para voltar quando fizer 18 anos
+
+if (idade === 17){
+    console.log('Volte quando tiver 18 anos.')
 }
 
