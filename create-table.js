dee8f5cff7c4ac8fddf066b55f1a2ffe561e6558
KI0T0 <f_g@outlook.com.br>
2024-09-17 08:22:53 -0300

    Removido pasta node_modules e ajustado o REST

--- create-table.js
+++ create-table.js
@@ -1,9 +1,15 @@
 import { sql } from './db.js'
 
+
+
+// sql`DROP TABLE IF EXISTS videos;`.then(() => {
+//     console.log('Tabela apagada!');
+// })
+
 sql`
 CREATE TABLE videos (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
+    id TEXT PRIMARY KEY,
+    title TEXT,
     description TEXT,
     duration INTEGER
 );
